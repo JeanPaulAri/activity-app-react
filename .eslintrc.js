871393765d97977250e9c48957081ba3770bd5b8
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-08-04 00:25:51 -0500

    none/ eslintrc.js, adding rule linebreak-style for especific OS

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
     'import/no-cycle': ['error', { maxDepth: Infinity }],
     'react/jsx-filename-extension':
         [1, { extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'] }],
